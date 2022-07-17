cc939795ceac841fdc565ddc058f609b3afd9f4f
Steve Lang <langloiss0003@my.uwstout.edu>
2022-07-17 17:45:47 -0500

    questions and some functions added

--- index.js
+++ index.js
@@ -1,13 +1,85 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is the title of the project?',
+        name: "title"
+    },
+    {
+        type: 'input',
+        message: 'What was your motivation?',
+        name: "motivation"
+    },
+    {
+        type: 'input',
+        message: 'Why did you build this project?',
+        name: 'build'
+    },
+    {
+        type: 'input',
+        message: 'What problem does it solve?',
+        name: 'problem'
+    },
+    {
+        type: 'input',
+        message: 'What did you learn?',
+        name: 'learn'
+    },
+    {
+        type: 'input',
+        message: 'What are the steps to install your project?',
+        name: 'install'
+    },
+    {
+        type: 'input',
+        message: 'To add a screenshot use ![alt text](assets/images/screenshot.png)',
+        name: 'image'
+    },
+    {
+        type: 'input',
+        message: 'List all the collaborators and the GitHub profiles',
+        name: 'collaborators'
+    },
+    {
+        type: 'input',
+        message: 'Did you use any third-party assets or follow any tutorials? ',
+        name: 'assets'
+    },
+    {
+        type: 'list',
+        message: 'Please choose a license.',
+        name: 'license',
+        choice: ['General Public License', 'Mozilla Public License', 'Apache License', 'MIT License', 'Boost Software License', 'Unlicense']
+    },
+    {
+        type: 'input',
+        message: 'Please list teh features for your project.',
+        name: 'features'
+    },
+    {
+        type: 'input',
+        message: 'How can others contribute to your project?',
+        name: 'contribute'
+    },
+    {
+        type: 'input',
+        message: 'How do you test your application?',
+        name: 'test'
+    },
+
+]).then((data) => {
+    console.log(data);
+})
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,127 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'General Public License') {
+    return "https://img.shields.io/badge/License-General--Public--License-brightgreen"
+  }
+  if (license === 'Mozilla Public License') {
+    return "https://img.shields.io/badge/License-Mozilla%20Public%20License-yellow"
+  }
+  if (license === 'Apache License') {
+    return "https://img.shields.io/badge/License-Apache--License-orange"
+  }
+  if (license === 'MIT License') {
+    return "https://img.shields.io/badge/License-MIT--License-red"
+  }
+  if (license === 'Boost Software License') {
+    return "https://img.shields.io/badge/License-Boost--Software--License-lightgrey"
+  }
+  if (license === 'Unlicense') {
+    return "";
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'General Public License') {
+    return "https://choosealicense.com/licenses/gpl-3.0/"
+  }
+  if (license === 'Mozilla Public License') {
+    return "https://choosealicense.com/licenses/mpl-2.0/"
+  }
+  if (license === 'Apache License') {
+    return "https://choosealicense.com/licenses/apache-2.0/"
+  }
+  if (license === 'MIT License') {
+    return "https://choosealicense.com/licenses/mit/"
+  }
+  if (license === 'Boost Software License') {
+    return "https://choosealicense.com/licenses/bsl-1.0/"
+  }
+  if (license === 'Unlicense') {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'General Public License') {
+    return "![General Public License]"
+  }
+  if (license === 'Mozilla Public License') {
+    return "![Mozilla Public License]"
+  }
+  if (license === 'Apache License') {
+    return "![Apache License]"
+  }
+  if (license === 'MIT License') {
+    return "![MIT License]"
+  }
+  if (license === 'Boost Software License') {
+    return "![Boost Software License]"
+  }
+  if (license === 'Unlicense') {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+
+  ## Description
+  -${data.motivation}
+  -${data.build}
+  -${data.problem}
+  -${data.learn}
+
+  ## Table of Contents
+
+[Installation](#installation)
+[Usage](#usage)
+[Credits](#credits)
+[License](#license)
+[Features](#features)
+[How to Contribute](#how to contribute)
+[Tests](#tests)
+
+## Installation
+Please follow these steps to install:
+-${data.install}
+
+## Usage
+Please follow to screenshots for the usage of the application:
+
+![alt text](assets/images/screenshot.png)
+
+## Credits
+List of collaborators on the application:
+
+-${data.collaborators}
+-${data.assets}
+
+## License
+
+-${renderLicenseSection(data.license)}${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
+
+## Features
+List of features on the applications:
+
+-${data.features}
+
+## How to Contribute
+Guidelines on howt to contribute to the application:
+
+-${data.contribute}
+
+## Tests
+Instruction on how to test the application:
+
+-${data.tests}
 `;
 }
 
